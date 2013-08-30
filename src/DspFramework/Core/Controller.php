870e1bab657f73208aacebb124b6ef0bf816a017
Pierre Feyssaguet <pfeyssaguet@gmail.com>
2013-08-30 22:28:40 +0200

    run method

--- src/DspFramework/Core/Controller.php
+++ src/DspFramework/Core/Controller.php
@@ -11,9 +11,28 @@ abstract class Controller
 		$methodName = $name . 'Action';
 		if (!method_exists($this, $methodName))
 		{
-			throw new ActionNotFoundException();
+			$message = "Action $name of controller " . get_class($this) . " does not exists";
+			$message .= " (method " . get_class($this) . "::$methodName not found)";
+			throw new ActionNotFoundException($message);
 		}
 
 		$response = call_user_func(array($this, $methodName), $args);
+	}
+
+	public static function run($appName)
+	{
+		$className = $appName . '\\' . $appName . 'FrontController';
+		if (!class_exists($className))
+		{
+			throw new \InvalidArgumentException("Class $className does not exists !");
+		}
+
+		if (!is_subclass_of($className, __CLASS__))
+		{
+			throw new \InvalidArgumentException("Class $className is not a subclass of " . __CLASS__ . " !");
+		}
+
+		$frontController = new $className();
+		$frontController->actionLaunch('default');
 	}
 }
